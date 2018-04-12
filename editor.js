cb187f697daf434342e29c76c8a9e536ab935333
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-12 16:40:14 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -109,21 +109,5 @@ function selected_color(event, x){
     map.data.overrideStyle(event.feature,{strokeColor: x});
     event.feature.setProperty("Color", x);
 }
-function dropDownMenu() {
-    document.getElementById("Dropdown").classList.toggle("show");
-}
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}

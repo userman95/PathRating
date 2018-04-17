9dc2d115d921e9d7ddf528f0d8cc57d43bf137b2
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-17 23:12:06 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -110,7 +110,7 @@ function resize() {
 
 function info_box(data){
    info_window = new google.maps.InfoWindow({
-    content: '<button onclick="selected_color(event.feature,1);" class="vbRow">Very Bad</button>',position: data.latLng
+    content: '<button onclick="selected_color(1);" class="vbRow">Very Bad</button>',position: data.latLng
 	
   });
    info_window.open(map);

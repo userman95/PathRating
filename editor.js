e39e884f3c9028713dd6a5f3b59b7e5bb1d49660
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-18 02:04:23 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -125,7 +125,10 @@ function resize() {
 }
 
 function info_box(data){
-   info_window = new google.maps.InfoWindow({
+   selected = selected.feature;
+    map.data.revertStyle();
+    map.data.overrideStyle(selected,{strokeWeight: 6});
+    info_window = new google.maps.InfoWindow({
     content: '<button onclick="Rating(rate);" class="vbRow">Very Bad</button>',position: data.latLng
 	
   });

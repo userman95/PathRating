850d86adc40cee9d6cb39828016684230acbbbfb
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-24 01:11:00 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -12,7 +12,7 @@ var array = [];
 function storeValue(){
         var bigOne = document.getElementById('bigOne');
         var dbref = firebase.database().ref();
-         dbref.child('text').set(geoJsonOutput.value);
+         dbref.set(geoJsonOutput.value);
 }
 function init() {
   // Initialise the map.

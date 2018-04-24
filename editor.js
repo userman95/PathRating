1b99045381b27b783897b8540ba14958169d2d6e
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-24 16:32:11 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -169,8 +169,12 @@ function info_box(data){
 }
 function openNav() {
     document.getElementById("mySidenav").style.width = "15%";
+    document.getElementById("main").style.marginLeft = "15%";
+
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+
 }
